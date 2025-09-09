9aab91cd22a6306a71a51e3ee0df3f572e039415
akashsonu0 <akashsonu7744@gmail.com>
2025-09-09 12:00:40 +0530

    Microservices

---
+++ SpringCloudCartService/src/main/java/in/pwskills/akash/rest/CartServiceController.java
@@ -0,0 +1,18 @@
+package in.pwskills.akash.rest;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v1/api/product")
+public class CartServiceController {
+	
+	@GetMapping("/cart")
+	public ResponseEntity<String>getProduct(){
+		
+		return ResponseEntity.ok("GETTING OUTPUT OF MS# THROUGH EUREKA SERVER...");
+	}
+
+}
