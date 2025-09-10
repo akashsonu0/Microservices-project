ef5b5cc2ace91e23b9d2bc3e3a796149527bf423
akashsonu0 <akashsonu7744@gmail.com>
2025-09-10 23:26:38 +0530

    MS comm using DiscoveryClient

--- SpringCloudCartService/src/main/java/in/pwskills/akash/rest/CartServiceController.java
+++ SpringCloudCartService/src/main/java/in/pwskills/akash/rest/CartServiceController.java
@@ -1,18 +1,30 @@
 package in.pwskills.akash.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/v1/api/product")
+@RequestMapping("/v1/api/cart")
 public class CartServiceController {
 	
-	@GetMapping("/cart")
+	@Value("${server.port}")
+	private String portNo;
+	
+	/*
+	 * METHOD : GET
+	 * PATH   : /show
+	 * OUTPUT : R.T<String>
+	 * URL    : /v1/api/cart/show
+	 * 
+	 */
+	
+	@GetMapping("/show")
 	public ResponseEntity<String>getProduct(){
 		
-		return ResponseEntity.ok("GETTING OUTPUT OF MS# THROUGH EUREKA SERVER...");
+		return ResponseEntity.ok("WELCOME TO CART SERVICE RUNNING ON :: "+portNo);
 	}
 
 }
