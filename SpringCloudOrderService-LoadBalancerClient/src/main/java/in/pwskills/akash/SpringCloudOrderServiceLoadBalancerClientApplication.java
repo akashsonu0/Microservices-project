ba631ad021d78301efbced16329fb32277e6c66f
akashsonu0 <akashsonu7744@gmail.com>
2025-09-11 17:27:25 +0530

    LoadBalanacerClient for MS-Communication

---
+++ SpringCloudOrderService-LoadBalancerClient/src/main/java/in/pwskills/akash/SpringCloudOrderServiceLoadBalancerClientApplication.java
@@ -0,0 +1,13 @@
+package in.pwskills.akash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudOrderServiceLoadBalancerClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudOrderServiceLoadBalancerClientApplication.class, args);
+	}
+
+}
