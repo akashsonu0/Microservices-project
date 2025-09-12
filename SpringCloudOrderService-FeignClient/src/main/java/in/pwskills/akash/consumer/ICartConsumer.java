bf26e84b0d83f84779c49bd3c2795eb52b517f90
akashsonu0 <akashsonu7744@gmail.com>
2025-09-12 20:14:06 +0530

    FeignClient with GET/GET{id}/POST(@RequestBody)

---
+++ SpringCloudOrderService-FeignClient/src/main/java/in/pwskills/akash/consumer/ICartConsumer.java
@@ -0,0 +1,27 @@
+package in.pwskills.akash.consumer;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import in.pwskills.akash.response.Cart;
+
+@FeignClient("CART-SERVICE")
+public interface ICartConsumer {
+	
+	@GetMapping("/v1/api/cart/show")
+	public ResponseEntity<String>getCardInfo();
+
+	@GetMapping("/v1/api/cart/find/{id}")
+	public ResponseEntity<Cart>getCartById(
+			@PathVariable Integer id);
+	
+	@PostMapping("/v1/api/cart/create")
+	public ResponseEntity<String> addCartDetails(
+			@RequestBody Cart cart);
+	
+
+}
---
+++ SpringCloudOrderService-FeignClient/src/main/java/in/pwskills/akash/response/Cart.java
@@ -0,0 +1,14 @@
+package in.pwskills.akash.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Cart {
+	private Integer cartId;
+	private String cartCode;
+	private Double cartCost;
+}
