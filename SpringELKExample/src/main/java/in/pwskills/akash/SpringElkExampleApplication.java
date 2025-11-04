f0313b809d0a6519d52c446b1a188210198f5f2a
akashsonu0 <akashsonu7744@gmail.com>
2025-11-04 15:36:29 +0530

    ELK

---
+++ SpringELKExample/src/main/java/in/pwskills/akash/SpringElkExampleApplication.java
@@ -0,0 +1,13 @@
+package in.pwskills.akash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringElkExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringElkExampleApplication.class, args);
+	}
+
+}
