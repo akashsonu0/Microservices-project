b75a51eefd3acafdb5335a93345649a00b9dccb8
akashsonu0 <akashsonu7744@gmail.com>
2025-11-04 14:44:58 +0530

    CircuitBreaker

---
+++ SpringCircuitBreakerApp/src/main/java/in/pwskills/akash/SpringCircuitBreakerAppApplication.java
@@ -0,0 +1,13 @@
+package in.pwskills.akash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCircuitBreakerAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCircuitBreakerAppApplication.class, args);
+	}
+
+}
---
+++ SpringMSIntercommApp/src/main/java/in/pwskills/akash/rest/OrderRestController.java
@@ -0,0 +1,15 @@
+package in.pwskills.akash.rest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class OrderRestController {
+	@GetMapping("/item")
+	public ResponseEntity<String> createOrder() {
+		return new ResponseEntity<String>("HELLO USERS!!", HttpStatus.OK);
+	}
+
+}
